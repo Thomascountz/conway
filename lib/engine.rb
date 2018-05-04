4861ffc2eccd557b26a2b4c95079ba7e7be3922c
Thomas Countz <thomascountz@gmail.com>
2018-05-04 10:53:06 -0400

    rely on messages, not data

--- lib/engine.rb
+++ lib/engine.rb
@@ -6,8 +6,13 @@ class Engine
 
   def play(num_of_generations)
     num_of_generations.times do
-      @renderer.render(@colony)
-      @colony = @colony.incubate
+      renderer.render(colony)
+      self.colony = colony.incubate
     end
   end
+
+  private
+
+  attr_accessor :colony
+  attr_reader :renderer
 end
